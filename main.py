c42111feb799f707039760404c1bf63f76c18637
Adolf David <143256974+adolfzcoder@users.noreply.github.com>
2024-12-22 13:47:52 +0200

    main

--- main.py
+++ main.py
@@ -9,26 +9,20 @@ from clean_data import clean_missed_data as cmd
 # reset.reset_txts()        
 # cd.clean_data() 
 # cmd.clean_missed_data()
-print("resetting the files")
-reset.reset_txts()    
-print("cleaning the data")
-cd.clean_data()
-
-
-print("Outputting the cleaned data to json")
-process.process_file()
-print("Done! All data cleaned and written to output file")
 
 
 
-
-# if '__name__' == '__main__':
-#     print("Running main...")
-#     # got annoying deleting data every single time i have to run it...so i just made a package for that or module idk, java messin me up fr
+if '__name__' == '__main__':
+    print("Running main...")
+    # got annoying deleting data every single time i have to run it...so i just made a package for that or module idk, java messin me up fr
+    print("resetting the files")
+    reset.reset_txts()    
+    print("cleaning the data")
+    cd.clean_data()
     
-#     reset.reset_txts()        
-#     cd.clean_data() 
-#     cmd.clean_missed_data()
-     
-#     process.process_file()
-#     print("Done! All data cleaned and written to output file")
+    
+    print("Outputting the cleaned data to json")
+    process.process_file()
+    print("Done! All data cleaned and written to output file")
+
+
